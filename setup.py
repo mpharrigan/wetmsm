62fd13c1ec01739399d0b4500586c0c179489301
Matthew Harrigan <harrigan@stanford.edu>
2014-07-09 16:01:58 -0700

    Compile

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from Cython.Build import cythonize
 
 setup(
         name='WetMSM',
-        #ext_modules = cythonize("cysingmodel/_ising.pyx"),
+        ext_modules = cythonize("wetmsm/*.pyx"),
         packages=['wetmsm']
 )
