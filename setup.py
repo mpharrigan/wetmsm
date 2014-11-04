e03d9cfdd6250d38c4f973201b7b654fa52a94dd
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2014-11-04 15:46:11 -0800

    added numpy requirement to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
-
+import numpy
 
 setup(
     name='WetMSM',
     version="0.2",
+    include_dirs=[numpy.get_include()],
     ext_modules=cythonize("wetmsm/*.pyx"),
     packages=find_packages(),
     zip_safe=False
