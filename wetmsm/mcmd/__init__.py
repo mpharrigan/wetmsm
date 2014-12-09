a935665f1d4aea86eb05b3cba5d6c85923349f44
Matthew Harrigan <harrigan@stanford.edu>
2014-12-08 17:53:22 -0800

    Remove custom command line wrapper

--- wetmsm/mcmd/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from .mcmd import Parsable, parsify
-
-__all__ = ['Parsable', 'parsify']
